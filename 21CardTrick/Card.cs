c4cd6f3b763e6a83fdf70f3307ff52677cd012b8
Rhett <notreal@example.com>
2019-01-23 12:33:19 -0700

    Finished Card and Player class.

--- 21CardTrick/Card.cs
+++ 21CardTrick/Card.cs
@@ -10,12 +10,40 @@ namespace _21CardTrick
     {
         private string suit;
         private string face;
+        private string image;
 
         public Card(string suit, string face)
         {
             this.suit = suit;
             this.face = face;
+
+            setImage();
+
         }
 
-    }
+        public string getSuit(){
+
+            return suit;
+
+        }
+
+        public string getface(){
+
+            return face;
+
+        }
+
+        public string getImage(){
+
+            return image;
+
+        }
+
+        private void setImage()
+        {
+            image = face + "_of_" + suit; 
+        }
+
+            
+     }
 }
