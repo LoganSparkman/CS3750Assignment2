64d97392534a4f2b855438a6c9bb2ce212254edd
Rhett <notreal@example.com>
2019-01-23 13:06:51 -0700

    Added Dealer and Deck classes for Andrew

---
+++ 21CardTrick/Dealer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21CardTrick
+{
+    class Dealer
+    {
+        private int dealNumber;
+        private int card;
+
+        public void deal(int dealNumber)
+        {
+            this.dealNumber = dealNumber;
+        }
+        public void revealCard(int card)
+        {
+            this.card = card;
+        }
+        public void pickupCards(int card)
+        {
+            this.card = card;
+        }
+
+    }
+}
---
+++ 21CardTrick/Deck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21CardTrick
+{
+    class Deck
+    {
+
+        private static readonly Random getrandom = new Random();
+
+        static public void shuffle(int[] deck)
+        {
+            for (int i = deck.Length - 1; i > 0; --i)
+            {
+                int k = getrandom.Next(i + 1);
+                int temp = deck[i];
+                deck[i] = deck[k];
+                deck[k] = temp;
+            }
+        }
+
+        public static int random21(int min, int max)
+        {
+            lock (getrandom)
+            {
+                return getrandom.Next(1, 21);
+            }
+        }
+
+    }
+}
