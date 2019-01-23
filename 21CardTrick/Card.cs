8cf9287c3ba81df6e4ab9bc0e432834a12543537
rhettsnodgrass <rhettsnodgrass@mail.weber.edu>
2019-01-23 10:36:43 -0700

    Created Card Class.

---
+++ 21CardTrick/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21CardTrick
+{
+    class Card
+    {
+        private string suit;
+        private string face;
+
+        public Card(string suit, string face)
+        {
+            this.suit = suit;
+            this.face = face;
+        }
+
+    }
+}
